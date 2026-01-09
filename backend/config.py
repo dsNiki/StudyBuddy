b84699c28974c3f31078b520ea61d36e7a421fae
dsNiki <nikidienes195@gmail.com>
2026-01-09 08:55:25 +0100

    database for public url

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,10 @@
 import os
 
 class Config:
-    # ENV-ből olvassa, local fallback-ként 'db'
+    # ENV-ből olvassa (Render/Railway), local fallback
+    db_url = os.getenv('DATABASE_URL') or os.getenv('MYSQL_URL') or 'mysql+pymysql://user:password@db:3306/studybuddy'
+    
+    if db_url.startswith('mysql://'):
+        db_url = db_url.replace('mysql://', 'mysql+pymysql://', 1)
+    
     SQLALCHEMY_DATABASE_URI
